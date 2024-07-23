a9229808fe3100dde895fd077ab4896704ca18ed
Njoku Ujunwa Sophia <106748148+Ujusophy@users.noreply.github.com>
2024-07-23 07:10:31 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+set -e
+
 echo "Updating package list and installing dependencies..."
 sudo apt-get update
 sudo apt-get install -y curl vim logrotate
-sudo cp devopsfetch.sh /usr/local/bin/devopsfetch.sh
-sudo chmod +x /usr/local/bin/devopsfetch.sh
+
+if [[ -f "devopsfetch.sh" ]]; then
+    echo "Copying devopsfetch.sh to /usr/local/bin..."
+    sudo cp devopsfetch.sh /usr/local/bin/devopsfetch.sh
+    sudo chmod +x /usr/local/bin/devopsfetch.sh
+else
+    echo "Error: devopsfetch.sh not found. Exiting."
+    exit 1
+fi
 
 echo "Creating systemd service file..."
 sudo tee /etc/systemd/system/devopsfetch.service <<EOF
@@ -38,7 +47,10 @@ sudo tee /etc/logrotate.d/devopsfetch <<EOF
     notifempty
     create 0640 root root
     postrotate
-        systemctl reload devopsfetch.service > /dev/null 2>/dev/null || true
+        # Restarting service only if necessary
+        if systemctl is-active --quiet devopsfetch.service; then
+            systemctl reload devopsfetch.service > /dev/null 2>/dev/null || true
+        fi
     endscript
 }
 EOF
